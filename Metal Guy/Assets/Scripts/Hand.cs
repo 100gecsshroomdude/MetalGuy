0bfbde93af9d5edd8d17fbffbf6a2bf12fa7739f
100gecsshroomdude <bissler@berkeley.edu>
2023-03-16 17:58:08 -0700

    Project2

---
+++ Metal Guy/Assets/Scripts/Hand.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Hand : MonoBehaviour
+{
+
+    [SerializeField] public InputActionReference controllerActionTrigger;
+    private XRDirectInteractor interactor;
+    private float prevTrigger = 0f;
+    private void Start()
+    {
+        interactor = GetComponent<XRDirectInteractor>();
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+
