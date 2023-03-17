976a1a4d76caabf7bf31e37d6331383660037bed
Bryan Wu <bryanwu@berkeley.edu>
2023-03-16 18:41:09 -0700

    first attempt at applying force through the hands

--- Metal Guy/Assets/Scripts/Hand.cs
+++ Metal Guy/Assets/Scripts/Hand.cs
@@ -4,18 +4,65 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Hand : MonoBehaviour {
 
+    #region Variables
     [SerializeField] public InputActionReference controllerActionTrigger;
     private XRDirectInteractor interactor;
     private float prevTrigger = 0f;
+    
+    private Rigidbody rb;
+    
+    private bool isTriggerPressed = false;
+    #endregion
+    
+    #region Methods
     private void Start()
     {
         interactor = GetComponent<XRDirectInteractor>();
+        rb = GetComponent<Rigidbody>();
     }
+
+
 
     void Update()
     {
+        if (controllerActionTrigger.action.triggered)
+        {
+            float trigger = controllerActionTrigger.action.ReadValue<float>();
+            if (trigger > 0.5f && prevTrigger <= 0.5f)
+            {
+                OnTriggerPressed();
+            }
+            else if (trigger <= 0.5f && prevTrigger > 0.5f)
+            {
+                OnTriggerReleased();
+            }
+            prevTrigger = trigger;
+        }
 
+        // Apply force to hand if trigger is pressed
+        if (isTriggerPressed)
+        {
+            rb.AddForce(transform.forward * 10f);
+        }
     }
 
+    private void OnTriggerPressed()
+    {
+        if (!isTriggerPressed)
+        {
+            // interactor.selectTarget?.SendMessage("OnTriggerPressed", SendMessageOptions.DontRequireReceiver);
+            isTriggerPressed = true;
+        }
+    }
+    private void OnTriggerReleased()
+    {
+        if (isTriggerPressed)
+        {
+            // interactor.selectTarget?.SendMessage("OnTriggerReleased", SendMessageOptions.DontRequireReceiver);
+            isTriggerPressed = false;
+        }
+    }
+
+    #endregion
 
 }
