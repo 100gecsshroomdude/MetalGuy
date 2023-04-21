25428ba05ed4fb11d5a8139549c69933b0a60489
100gecsshroomdude <bissler@berkeley.edu>
2023-04-20 18:09:17 -0700

    Initial bug fix for hand trigger detection and working applied force to player

--- Metal Guy/Assets/Scripts/Hand.cs
+++ Metal Guy/Assets/Scripts/Hand.cs
@@ -6,6 +6,7 @@ public class Hand : MonoBehaviour {
 
     #region Variables
     [SerializeField] public InputActionReference controllerActionTrigger;
+    [SerializeField] private Rigidbody playerRb;
     private XRDirectInteractor interactor;
     private float prevTrigger = 0f;
     
@@ -25,42 +26,36 @@ public class Hand : MonoBehaviour {
 
     void Update()
     {
-        if (controllerActionTrigger.action.triggered)
+        float trigger = controllerActionTrigger.action.ReadValue<float>();
+        print(trigger + ", " + prevTrigger);
+        if (trigger > 0.5f && prevTrigger <= 0.5f)
         {
-            float trigger = controllerActionTrigger.action.ReadValue<float>();
-            if (trigger > 0.5f && prevTrigger <= 0.5f)
-            {
-                OnTriggerPressed();
-            }
-            else if (trigger <= 0.5f && prevTrigger > 0.5f)
-            {
-                OnTriggerReleased();
-            }
-            prevTrigger = trigger;
+            OnTriggerPressed();
         }
+        else if (trigger <= 0.5f && prevTrigger > 0.5f)
+        {
+            print("releasing trigger");
+            OnTriggerReleased();
+        }
+        prevTrigger = trigger;
 
         // Apply force to hand if trigger is pressed
         if (isTriggerPressed)
         {
-            rb.AddForce(transform.forward * 10f);
+            playerRb.AddForce(transform.forward * 10f);
         }
     }
 
     private void OnTriggerPressed()
     {
-        if (!isTriggerPressed)
-        {
-            // interactor.selectTarget?.SendMessage("OnTriggerPressed", SendMessageOptions.DontRequireReceiver);
-            isTriggerPressed = true;
-        }
+        // interactor.selectTarget?.SendMessage("OnTriggerPressed", SendMessageOptions.DontRequireReceiver);
+        isTriggerPressed = true;
     }
     private void OnTriggerReleased()
     {
-        if (isTriggerPressed)
-        {
-            // interactor.selectTarget?.SendMessage("OnTriggerReleased", SendMessageOptions.DontRequireReceiver);
-            isTriggerPressed = false;
-        }
+        // interactor.selectTarget?.SendMessage("OnTriggerReleased", SendMessageOptions.DontRequireReceiver);
+        isTriggerPressed = false;
+
     }
 
     #endregion
