ab4edbc9538cc3a31e36ec060bdcb0c0b0caf69a
100gecsshroomdude <bissler@berkeley.edu>
2023-04-27 18:30:32 -0700

    New and improved

---
+++ Metal Guy/Assets/Scripts/Reset.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 currentSpawnPoint;
+    [SerializeField] public InputActionReference yvalue;
+    [SerializeField] public InputActionReference xvalue;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Respawn()
+    {
+        // Set the position to the spawn point.
+        player.transform.position = currentSpawnPoint;
+
+        // Zero out any left over velocity.
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().rotation = Quaternion.identity;
+    }
+    private void Orient()
+    {
+        player.GetComponent<Rigidbody>().rotation = Quaternion.identity;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var resetbutton = yvalue.action.ReadValue<float>();
+        var orientbutton = xvalue.action.ReadValue<float>();
+       
+
+
+        if (resetbutton>0)
+        {
+            Respawn();
+           
+        }
+
+        if (orientbutton>0)
+        {
+            Orient();
+
+        }
+        if ((player.transform.position.x > 460)|| (player.transform.position.x < -460)|| (player.transform.position.z > 460) || (player.transform.position.z < -460)||(player.transform.position.y < -10))
+        {
+            Respawn();
+        }
+    }
+    }
+
+    
